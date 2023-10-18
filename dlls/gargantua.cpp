c0226dbca537d2f87a36bc85b20eccc112aa2dae
Alivebyte <alivebyte.yt@gmail.com>
2023-10-18 09:59:15 +0300

    CBoss blood color fix

--- dlls/gargantua.cpp
+++ dlls/gargantua.cpp
@@ -1346,7 +1346,7 @@ void CBoss::Spawn()
 
 	pev->solid = SOLID_SLIDEBOX;
 	pev->movetype = MOVETYPE_STEP;
-	m_bloodColor = DONT_BLEED; // BLOOD_COLOR_GREEN
+	m_bloodColor = BLOOD_COLOR_GREEN;
 	pev->health = gSkillData.gargantuaHealth;
 	//pev->view_ofs		= Vector ( 0, 0, 96 );// taken from mdl file
 	m_flFieldOfView = -0.2;// width of forward view cone ( as a dotproduct result )
